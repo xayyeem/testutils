be115582f70735ce739a8cfcde97f9178e43b748
xayyeem <kjafri30izback@gmail.com>
2023-07-13 18:26:11 +0530

    Updates

---
+++ precache-manifest.e7fc03ec1ffd69d297566d0a8ae3dc2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f25001d7e4249c4a7fdb",
+    "url": "/testutils/static/css/main.9ec9eec4.chunk.css"
+  },
+  {
+    "revision": "f25001d7e4249c4a7fdb",
+    "url": "/testutils/static/js/main.f25001d7.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/testutils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/testutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "814121f9b255874eeba2",
+    "url": "/testutils/static/js/runtime~main.814121f9.js"
+  },
+  {
+    "revision": "90ae947d04e1ce493fd095b82cf046bc",
+    "url": "/testutils/index.html"
+  }
+];
